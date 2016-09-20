fe5dad4b6d5759d815091fa02e2e451d5a886040
Jani Yli-Kantola <jani.ylikantola@gmail.com>
2016-09-20 15:37:28 +0300

    UI Emulator - Particulars & Contacts

--- Account/operator_emulator/ui_emulator.py
+++ Account/operator_emulator/ui_emulator.py
@@ -28,6 +28,9 @@ account_host = account_ip+":"+account_port
 headers = {'Content-Type': 'application/json'}
 
 account_id = ""
+particular_id = ""
+contacts_id = ""
+
 username = "example_username-" + str(uuid4())
 password = "example_password"
 
@@ -37,11 +40,52 @@ account_template = {
         "attributes": {
             'firstName': 'ExampleFirstName',
             'lastName': 'ExampleLastName',
-            'dateOfBirth': '201-05-14',
+            'dateOfBirth': '2010-05-14',
             'email': username + '@examlpe.org',
             'username': username,
             'password': password,
             'acceptTermsOfService': 'True'
+        }
+    }
+}
+
+particular_template_for_patch = {
+    "data": {
+        "type": "Particular",
+        "attributes": {
+            'lastname': 'NewExampleLastName'
+        }
+    }
+}
+
+contact_template = {
+    "data": {
+        "type": "Contact",
+        "attributes": {
+            'address1': 'Example address 1',
+            'address2': 'Example address 2',
+            'postalCode': '97584',
+            'city': 'Example city',
+            'state': 'Example state',
+            'country': 'Example country',
+            'type': 'Personal',
+            'primary': 'True'
+        }
+    }
+}
+
+contact_template_for_patch = {
+    "data": {
+        "type": "Contact",
+        "attributes": {
+            'address1': 'Example address 1',
+            'address2': 'Example address 2',
+            'postalCode': '65784',
+            'city': 'Example city',
+            'state': 'Example state',
+            'country': 'Example country',
+            'type': 'Personal',
+            'primary': 'False'
         }
     }
 }
@@ -61,8 +105,8 @@ def post(host=None, endpoint=None, headers=None, data=None):
         raise AttributeError("Provide data as parameter")
 
     url = host + endpoint
-    print("Request")
     print("Endpoint: " + endpoint)
+    print("Headers: " + json.dumps(headers))
     print("Payload: " + json.dumps(data))
 
     req = requests.post(url, headers=headers, json=data)
@@ -72,7 +116,29 @@ def post(host=None, endpoint=None, headers=None, data=None):
     return status_code, response_data
 
 
-def get(host=None, endpoint=None, headers=None):
+def patch(host=None, endpoint=None, headers=None, data=None):
+    if host is None:
+        raise AttributeError("Provide host as parameter")
+    if endpoint is None:
+        raise AttributeError("Provide endpoint as parameter")
+    if headers is None:
+        raise AttributeError("Provide headers as parameter")
+    if data is None:
+        raise AttributeError("Provide data as parameter")
+
+    url = host + endpoint
+    print("Endpoint: " + endpoint)
+    print("Headers: " + json.dumps(headers))
+    print("Payload: " + json.dumps(data))
+
+    req = requests.patch(url, headers=headers, json=data)
+    status_code = str(req.status_code)
+    response_data = json.loads(req.text)
+
+    return status_code, response_data
+
+
+def get(host=None, endpoint=None, headers=None, username=None, password=None):
     if host is None:
         raise AttributeError("Provide host as parameter")
     if endpoint is None:
@@ -81,10 +147,13 @@ def get(host=None, endpoint=None, headers=None):
         raise AttributeError("Provide headers as parameter")
 
     url = host + endpoint
-    print("Request")
     print("Endpoint: " + endpoint)
+    print("Headers: " + json.dumps(headers))
 
-    req = requests.get(url, headers=headers)
+    if username is not None and password is not None:
+        req = requests.get(url, headers=headers, auth=HTTPBasicAuth(username=username, password=password))
+    else:
+        req = requests.get(url, headers=headers)
     status_code = str(req.status_code)
     response_data = json.loads(req.text)
 
@@ -93,6 +162,7 @@ def get(host=None, endpoint=None, headers=None):
 
 ######### Actions
 
+#
 # Create Account
 print ("------------------------------------")
 title = "Create Account"
@@ -102,27 +172,142 @@ try:
 except Exception as exp:
     print(title + ": " + repr(exp))
 else:
-    request_statuses.append(title + ": " + account[0])
+    request_response = title + ": " + account[0] + ": " + json.dumps(account[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
     account_id = str(account[1]['data'].get("id", "None"))
     print ("Response " + account[0] + ": " + json.dumps(account[1]))
     print ("Account ID: " + account_id)
 
-# Particulars
+#
+# Authenticate
+print ("------------------------------------")
+title = "Authenticate"
+print(title)
+try:
+    api_auth = get(host=account_host, endpoint="/api/auth/user/", headers=headers, username=username, password=password)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_response = title + ": " + api_auth[0] + ": " + json.dumps(api_auth[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    apikey = str(api_auth[1].get("Api-Key", "None"))
+    headers['Api-Key'] = apikey
+    print ("Response " + api_auth[0] + ": " + json.dumps(api_auth[1]))
+    print ("apikey: " + apikey)
+
+#
+# PARTICULARS
 print ("------------------------------------")
 title = "List Particulars"
 print(title)
 try:
-    account = get(host=account_host, endpoint="/api/accounts/" + account_id + "/particulars/", headers=headers)
+    particulars = get(host=account_host, endpoint="/api/accounts/" + account_id + "/particulars/", headers=headers)
 except Exception as exp:
     print(title + ": " + repr(exp))
 else:
-    request_statuses.append(title + ": " + account[0])
-    particular_id = str(account[1]['data'].get("id", "None"))
-    print ("Response " + account[0] + ": " + json.dumps(account[1]))
+    request_response = title + ": " + particulars[0] + ": " + json.dumps(particulars[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    particular_id = str(particulars[1]['data'][0].get("id", "None"))
+    print ("Response " + particulars[0] + ": " + json.dumps(particulars[1]))
     print ("particular_id: " + particular_id)
 
-# REPORT
+
+print ("------------------------------------")
+title = "Update Particular"
+print(title)
+try:
+    particular_template_for_patch['data']['id'] = str(particular_id)
+    updated_particular = patch(host=account_host, endpoint="/api/accounts/" + account_id + "/particulars/" + particular_id + "/", headers=headers, data=particular_template_for_patch)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_response = title + ": " + updated_particular[0] + ": " + json.dumps(updated_particular[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    print ("Response " + updated_particular[0] + ": " + json.dumps(updated_particular[1]))
+
+
+print ("------------------------------------")
+title = "One Particular"
+print(title)
+try:
+    particular = get(host=account_host, endpoint="/api/accounts/" + account_id + "/particulars/" + particular_id + "/", headers=headers)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_response = title + ": " + particular[0] + ": " + json.dumps(particular[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    print ("Response " + particular[0] + ": " + json.dumps(particular[1]))
+    print ("particular_id: " + str(particular[1]['data'].get("id", "None")))
+
+
+#
+# CONTACTS
+print ("------------------------------------")
+title = "Add Contact"
+print(title)
+try:
+    new_contact = post(host=account_host, endpoint="/api/accounts/" + account_id + "/contacts/", headers=headers, data=contact_template)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_response = title + ": " + new_contact[0] + ": " + json.dumps(new_contact[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    print ("Response " + new_contact[0] + ": " + json.dumps(new_contact[1]))
+
+print ("------------------------------------")
+title = "List Contacts"
+print(title)
+try:
+    contacts = get(host=account_host, endpoint="/api/accounts/" + account_id + "/contacts/", headers=headers)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_response = title + ": " + contacts[0] + ": " + json.dumps(contacts[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    contacts_id = str(contacts[1]['data'][0].get("id", "None"))
+    print ("Response " + contacts[0] + ": " + json.dumps(contacts[1]))
+    print ("contacts_id: " + contacts_id)
+
+
+print ("------------------------------------")
+title = "Update Contact"
+print(title)
+try:
+    contact_template_for_patch['data']['id'] = str(contacts_id)
+    updated_contact = patch(host=account_host, endpoint="/api/accounts/" + account_id + "/contacts/" + contacts_id + "/", headers=headers, data=contact_template_for_patch)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_response = title + ": " + updated_contact[0] + ": " + json.dumps(updated_contact[1])
+    print('request_response: ' + request_response)
+    request_statuses.append(request_response)
+    print ("Response " + updated_contact[0] + ": " + json.dumps(updated_contact[1]))
+
+print ("------------------------------------")
+title = "One Contact"
+print(title)
+try:
+    contact = get(host=account_host, endpoint="/api/accounts/" + account_id + "/contacts/" + contacts_id + "/", headers=headers)
+except Exception as exp:
+    print(title + ": " + repr(exp))
+else:
+    request_statuses.append(title + ": " + contact[0] + ": " + json.dumps(contact[1]))
+    print ("Response " + contact[0] + ": " + json.dumps(contact[1]))
+    print ("contacts_id: " + str(contact[1]['data'].get("id", "None")))
+
+
+##########
+# REPORT #
+##########
 print ("------------------------------------")
 print("Request report")
 for request in request_statuses:
     print(request)
+
