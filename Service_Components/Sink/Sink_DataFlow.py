561553b1c09d4c603151653201e1fc4a19b80579
Allu2 <aleksi.ajp@gmail.com>
2016-09-27 14:05:16 +0300

    Added comment template for starting implementation to Sink and Source modules.

--- Service_Components/Sink/Sink_DataFlow.py
+++ Service_Components/Sink/Sink_DataFlow.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'alpaloma'
 
-from flask import Blueprint, make_response
+from DetailedHTTPException import error_handler
+from flask import Blueprint
 from flask_restful import Resource, Api
-from DetailedHTTPException import DetailedHTTPException, error_handler
 
 api_Sink_blueprint = Blueprint("api_Sink_blueprint", __name__)
 api = Api()
@@ -25,6 +25,65 @@ class DataFlow(Resource):
         status = {"status": "running", "service_mode": "Sink"}
         return status
 
+    class DataFlow(Resource):
+        @error_handler
+        def get(self, user_id, cr_id, rs_id):
+
+            # Create request
+            # Validate CR
+            # Check integrity (signature)
+            # Check that state is "Active"
+            # Check "Issued" timestamp
+            # Check "Not Before" timestamp
+            # Check "Not After" timestamp
+            # CR validated.
+
+            # Validate Request from UI
+            # Check that rs_description field contains rs_id
+            # Check that rs_description field contains data_set_id (Optional?)
+            # Request from UI validated.
+
+            # Fetch data request urls
+            # LOOP: for every data_set_id
+                # Fetch corresponding distrubution point url based on data_set_id
+            # Data request urls fetched.
+
+            # Validate Authorisation Token
+            # Fetch Authorisation Token related CR from data storage by rs_id
+            # Check Integrity ( Signed by operator, Operator's public key can be found from SLR)
+            # Check that "sub" contains correct public key (operators, sources?)
+            # Check "Issued" timestamp
+            # Check "Not Before" timestamp
+            # Check "Not After" timestamp
+
+            # OPT: Token expired
+            # Get new Authorization token, start agian from validation.
+
+            # Check URL patterns in "aud" field
+            # Check that fetched distribution urls can be found from "aud" field
+
+            # Token validated
+            # With these two steps Sink has verified that it's allowed to make request.
+
+            # Construct request
+            # Select request URL from "aud" field
+            # Add Authorisation Token to request
+            # Request constructed.
+
+            # Sign request
+            # Fetch private key pair of public key specified in Authorisation Token's "sub" field.
+            # Sign with fetched private key
+            # Sign with fetched private key
+            # Add signature to request
+            # Request signed.
+            # Request created.
+
+            # Make Data Request
+
+            status = {"status": "running", "service_mode": "Sink"}
+            return status
+
+
 
 api.add_resource(DataFlow, '/init')
-
+api.add_resource(DataFlow, ' /user/<user_id>/consentRecord/<cr_id>/resourceSet/<rs_id>')
--- Service_Components/Source/Source_DataFlow.py
+++ Service_Components/Source/Source_DataFlow.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'alpaloma'
 
-from flask import Blueprint, make_response
+from DetailedHTTPException import error_handler
+from flask import Blueprint
 from flask_restful import Resource, Api
-from DetailedHTTPException import DetailedHTTPException, error_handler
 
 api_Source_blueprint = Blueprint("api_Source_blueprint", __name__)
 api = Api()
@@ -19,12 +19,41 @@ api.init_app(api_Source_blueprint)
 #         resp.headers.extend(headers)
 #         return resp
 
-class DataFlow(Resource):
+class Status(Resource):
     @error_handler
     def get(self):
         status = {"status": "running", "service_mode": "Source"}
         return status
 
+class DataRequest(Resource):
+    @error_handler
+    def get(self):
 
-api.add_resource(DataFlow, '/init')
+        # Validate Request
+        # Validate Token
+        # Check that related Consent Record exists with the same rs_id
+        # Check that auth_token_issuer_key field of CR matches iss-field in Authorization token
+        # Check Token's integrity against the signature
+        # Check Token's validity period includes time of data request
+        # Check Token's "aud" field includes the URI to which the data request was made
+        # Token validated.
 
+        # Validate request
+        # Check that request was signed with the key in the Token
+        # Request validated.
+
+        # Validate related CR
+        # Validate the related Consent Record as defined in MyData Authorisation Specification
+        # CR Validated.
+
+        # OPT: Introspection
+        # GET Consent Record Status (source_cr_id)
+
+        # Process request
+        # Return.
+
+        status = {"status": "running", "service_mode": "Source"}
+        return status
+
+api.add_resource(DataRequest, '/datarequest')
+api.add_resource(Status, '/init')
