92b51229751b2a033d2c06122577a51e3ca3fc24
Aleksi Palomäki <aleksi.ajp@gmail.com>
2016-11-28 15:34:04 +0200

    Refactor introspection.py

--- Operator_Components/Operator_CR/introspection.py
+++ Operator_Components/Operator_CR/introspection.py
@@ -6,7 +6,6 @@ from DetailedHTTPException import error_handler, DetailedHTTPException
 from flask import Blueprint, current_app
 from flask_restful import Api, Resource
 from helpers import AccountManagerHandler
-from helpers import Helpers
 
 # Init Flask
 api_CR_blueprint = Blueprint("api_Introspection_blueprint", __name__)
@@ -16,6 +15,8 @@ api.init_app(api_CR_blueprint)
 # Logging
 debug_log = logging.getLogger("debug")
 logger = logging.getLogger("sequence")
+
+
 class Introspection(Resource):
     def __init__(self):
         super(Introspection, self).__init__()
@@ -26,29 +27,30 @@ class Introspection(Resource):
         try:
             self.AM = AccountManagerHandler(self.am_url, self.am_user, self.am_password, self.timeout)
         except Exception as e:
-            debug_log.warn("Initialization of AccountManager failed. We will crash later but note it here.\n{}".format(repr(e)))
-        helper_object = Helpers(current_app.config)
+            debug_log.warn("Initialization of AccountManager failed. We will crash later but note it here.\n{}"
+                           .format(repr(e)))
 
     @error_handler
     def get(self, cr_id):
         '''post
 
-        :return: Returns latest csr for source
+        :return: Returns latest csr id for source
         '''
         try:
-            debug_log.info("We received introspection request for cr_id ({})".format(cr_id))
+            debug_log.info("We received request for latest csr id for cr_id ({})".format(cr_id))
             result = self.AM.get_last_csr(cr_id)
         except AttributeError as e:
             raise DetailedHTTPException(status=502,
                                         title="It would seem initiating Account Manager Handler has failed.",
                                         detail="Account Manager might be down or unresponsive.",
                                         trace=traceback.format_exc(limit=100).splitlines())
-        debug_log.info(dumps(result))
+        debug_log.info("Latest csr id for given cr_id is: ".format(result["csr_id"]))
         return result
 
-class Introspection_Missing(Resource):
+
+class IntrospectionMissing(Resource):
     def __init__(self):
-        super(Introspection_Missing, self).__init__()
+        super(IntrospectionMissing, self).__init__()
         self.am_url = current_app.config["ACCOUNT_MANAGEMENT_URL"]
         self.am_user = current_app.config["ACCOUNT_MANAGEMENT_USER"]
         self.am_password = current_app.config["ACCOUNT_MANAGEMENT_PASSWORD"]
@@ -56,15 +58,15 @@ class Introspection_Missing(Resource):
         try:
             self.AM = AccountManagerHandler(self.am_url, self.am_user, self.am_password, self.timeout)
         except Exception as e:
-            debug_log.warn("Initialization of AccountManager failed. We will crash later but note it here.\n{}".format(repr(e)))
-        helper_object = Helpers(current_app.config)
+            debug_log.warn("Initialization of AccountManager failed. We will crash later but note it here.\n{}"
+                           .format(repr(e)))
 
     @error_handler
     def get(self, cr_id, csr_id):
-        '''get
+        """get
 
         :return: Returns latest csr for source
-        '''
+        """
         try:
             debug_log.info("We received introspection request for cr_id ({})".format(cr_id))
             result = self.AM.get_missing_csr(cr_id, csr_id)
@@ -77,4 +79,4 @@ class Introspection_Missing(Resource):
         return result
 
 api.add_resource(Introspection, '/introspection/<string:cr_id>')
-api.add_resource(Introspection_Missing, '/consent/<string:cr_id>/missing_since/<string:csr_id>')
+api.add_resource(IntrospectionMissing, '/consent/<string:cr_id>/missing_since/<string:csr_id>')
