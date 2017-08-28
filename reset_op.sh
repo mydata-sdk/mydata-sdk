dec06c4bf04dbcd80d5ea0102f443b313c005fb3
Aleksi Palomäki <aleksi.ajp@gmail.com>
2017-08-28 18:19:07 +0300

    Cleaning unused files.

--- reset_op.sh
+++
@@ -1,4 +0,0 @@
-docker-compose kill operator_components
-docker-compose rm --force operator_components       # Clean db
-#docker volume rm mydatasdkbleedingedge_mysql-data  # Clean db
-docker-compose up -d operator_components
